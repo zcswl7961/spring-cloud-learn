69ed9d43340ab2afaefbe3365b9504c487b4d1c3
chenggong zhou <26921832@qq.com>
2019-11-20 13:45:53 +0800

    :sparkles: spring-cloud zipkin使用

---
+++ spring-eureka-framework/spring-zipkin-server/README.md
@@ -0,0 +1,8 @@
+## zipkin server
+spring cloud版本在F版本（之后）的时候，已经不需要自己构建zipkin server。如果在F版本之前，可以参考：
+[spring cloud C版本构建zipkin server](https://blog.csdn.net/m15231417197/article/details/81540829)
+进行构建</br>
+spring cloud G版本的 zipkin server 构建参考：[zipkin server 构建](https://blog.csdn.net/wsgsm/article/details/83900135)</br>
+[https://blog.csdn.net/zsf4748/article/details/80852264](https://blog.csdn.net/zsf4748/article/details/80852264)
+
+
--- spring-eureka-framework/spring-zuul-clusterA/src/main/java/com/zcswl/spring/zuul/ZuulApplication.java
+++ spring-eureka-framework/spring-zuul-clusterA/src/main/java/com/zcswl/spring/zuul/ZuulApplication.java
@@ -1,11 +1,8 @@
 package com.zcswl.spring.zuul;
 
-import com.zcswl.spring.zuul.filter.MyFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
@@ -15,7 +12,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
-@EnableEurekaClient
 public class ZuulApplication {
 
 
