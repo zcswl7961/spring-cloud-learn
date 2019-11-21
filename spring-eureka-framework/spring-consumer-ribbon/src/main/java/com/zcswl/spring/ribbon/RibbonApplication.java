d2e904413c61be546ea8926d7057538ae8e9563a
chenggong zhou <26921832@qq.com>
2019-11-21 09:20:43 +0800

    :sparkles: hystrix dashboard 安装操作

--- spring-eureka-framework/spring-consumer-ribbon/src/main/java/com/zcswl/spring/ribbon/RibbonApplication.java
+++ spring-eureka-framework/spring-consumer-ribbon/src/main/java/com/zcswl/spring/ribbon/RibbonApplication.java
@@ -1,10 +1,14 @@
 package com.zcswl.spring.ribbon;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,6 +27,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class RibbonApplication {
 
     public static void main(String[] args) {
@@ -39,6 +45,16 @@ public class RibbonApplication {
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 
 }
