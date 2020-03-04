c65932ae51bc1cae1634a6bac652562100281829
chenggong zhou <26921832@qq.com>
2020-03-04 18:55:35 +0800

    netty研究

---
+++ spring-netty-framework/src/main/java/com/zcswl/netty/client/Client.java
@@ -0,0 +1,60 @@
+package com.zcswl.netty.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * @author zhoucg
+ * @date 2020-03-04 16:22
+ */
+public class Client {
+
+
+    public void connect(int port, String host) {
+        EventLoopGroup worker = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            /**
+             * EventLoop组
+             */
+            bootstrap.group(worker);
+
+            /**
+             * 用于构建socketChannel工厂
+             */
+            bootstrap.channel(NioSocketChannel.class);
+            /**设置选项
+             * 参数：Socket的标准参数（key，value），可自行百度
+             保持呼吸，不要断气！
+             * */
+            bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
+
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                public void initChannel(SocketChannel ch) throws Exception {
+                    ch.pipeline().addLast(new SimpleClientHandler());
+                }
+            });
+            /** 开启客户端监听*/
+            ChannelFuture f = bootstrap.connect(host, port).sync();
+            /**等待数据直到客户端关闭*/
+            f.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            worker.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client=new Client();
+        client.connect(9999,"127.0.0.1");
+    }
+}
